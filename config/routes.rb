58da19765639b2136d30a03fb5746296281c1777
abxcoder <abxcoder@gmail.com>
2022-06-30 13:41:44 +0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,7 @@ Rails.application.routes.draw do
   resources :restorans
   root "pages#dashboard"
   get 'pagesdashboard' => 'pages#dashboard'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
@@ -18,8 +15,7 @@ Rails.application.routes.draw do
   get '/persons' => 'person#index'
   get '/persons/:id/edit' => 'person#edit', as: "edit_member"
   patch '/persons/:id' => 'person#update', as: "update_member"
+
   
-#   devise_for :users, :path_prefix => 'd'
-# resources :users, :only =>[:show]
 
 end
