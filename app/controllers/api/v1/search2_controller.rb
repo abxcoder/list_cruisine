477dfbc06f4f97d588ffe86a48ccbb8874fe3bda
abxcoder <abxcoder@gmail.com>
2022-07-15 14:30:51 +0700

    optimalisasi search2

--- app/controllers/api/v1/search2_controller.rb
+++ app/controllers/api/v1/search2_controller.rb
@@ -14,15 +14,50 @@ class Api::V1::Search2Controller < ApiController
 
           @id_food = Food.where(kategori_id: @kategori[0].id).pluck(:id)
           @menu = Menu.where(menus: {food_id: @id_food})
-          @id_restoran = @menu.pluck(:restoran_id).uniq
-          @restoran = Restoran.where(restorans: {id: @id_restoran})
 
+          if @menu.count > 0
 
-          render json: { "Kategori": @food, "id": @id_food, menunya: @menu, id_restoran: @id_restoran, restoran: @restoran}, status: :ok
+            @final_resto = Array.new
+            @nama_resto = Array.new
+            @nama_menu = Array.new
+            @nama_menus = Array.new
+            @final_kategori = Array.new
+
+            @id_restoran = @menu.pluck(:restoran_id).uniq
+            @restoran = Restoran.where(restorans: {id: @id_restoran})
+
+            @rt = 0
+            while @rt < @restoran.count do
+              @nama_resto.push(@restoran[@rt])
+              @mn = 0 
+              while @mn < @menu.count do
+                if @restoran[@rt].id == @menu[@mn].restoran_id 
+                  @nama_menu.push(@menu[@mn])
+                  @nama_menus.push(@nama_menu)
+                end
+                @mn += 1
+                @nama_menu = []
+              end
+              @final_resto.push("restoran": @nama_resto, menu: @nama_menus)
+              @rt += 1
+              @nama_resto = []
+              @nama_menus = []
+            end
+
+            
+            
+            render json: { "final_resto": @final_resto, "final_kategori": @final_kategori}, status: :ok
+
+          else
+
+            api.push(keyword: @kategori, result: "Maaf, menu yang anda cari tidak ditemukan")
+            render json: { data: api }, status: :ok
+
+          end
 
         else
 
-            api.push(keyword: @kategori, result: "Maaf, menu yang anda cari tidak ditemukan")
+            api.push(keyword: @kategori, result: "Maaf, Kategori yang anda cari tidak ditemukan")
             render json: { data: api }, status: :ok
 
         end
